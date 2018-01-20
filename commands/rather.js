4f88806092eca654e712eb9f7d9d6d773d112f8e
Jellz <xdjellz@gmail.com>
2018-01-20 19:25:27 +1100

    false error message

--- commands/rather.js
+++ commands/rather.js
@@ -4,8 +4,6 @@ module.exports = {
         const r = await snek.get('https://jellz.me/api/rather');
         const choices = JSON.parse(r.text);
         msg.channel.send(`**${choices.question}**\nA) ${choices.choicea}                           B) ${choices.choiceb}`);
-        msg.channel.send('\\❗ An unexpected error has occurred. Don\'t worry! The error has been automatically reported to bot administrators. If you try again and still see this message, wait for a fix. Join the support server found in `;about` for more information.');            
-        client.error(client, err.stack, `Command Run`, `**cmd:** ${msg.content} **user:** ${msg.author.tag} (${msg.author.id}) **guild:** ${msg.guild.name} (${msg.guild.id})`);
     },
     meta: {
         name: 'rather',
