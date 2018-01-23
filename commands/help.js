0fe19c63e863c1aef8cf04ce5cdb3f1fd88bbc2c
Jellz <xdjellz@gmail.com>
2018-01-23 23:50:23 +1100

    add some basic info to help command

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,23 @@
 module.exports = {
     run: async (client, msg, args) => {
-        msg.channel.send('hi nerd');
+        if (!args[0]) {
+            const helpMain = [
+                '**__Sky Main Help__**',
+                '',
+                `For a list of commands, use \`${client.config.prefix}commands\`.`,
+                'For in-depth information about a specific command, such as usage & description',
+                `please use \`${client.config.prefix}cmdinfo <command>\`.`,
+                '',
+                '**Some common help categories are...**',
+                `${client.config.prefix}help points`,
+                '',
+                'For live support, please join the support guild:',
+                'https://discord.gg/s9qed5A',
+                '... and we\'ll be happy to help.'
+            ].join('\n');
+            msg.channel.send(helpMain);
+        }
+        
     },
     meta: {
         name: 'help',
