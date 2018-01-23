4122dcb6ec5031b7b241c33ef2e839da51b4ec2b
Jellz <xdjellz@gmail.com>
2018-01-24 00:52:55 +1100

    add cleverbot help

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
         const categories = ['points'];
         if (!args[0]) {
             const helpMain = [
-                '**__Sky Main Help__**',
+                '__Help for **Sky** __',
                 '',
                 `For a list of commands, use \`${client.config.prefix}commands\`.`,
                 'For in-depth information about a specific command, such as usage & description',
@@ -17,10 +17,10 @@ module.exports = {
                 '... and we\'ll be happy to help.'
             ].join('\n');
             msg.channel.send(helpMain);
-        } else if (args[0]) {
+        } else if (args[0].toLowerCase() === 'points' || args[0].toLowerCase() === 'pts') {
             const pointsCmds = [`\`${client.config.prefix}lb\``, `\`${client.config.prefix}points\``, `\`${client.config.prefix}bet\``];
             const helpPoints = [
-                '**__Sky Points Help__**',
+                '__Help for **Points**__',
                 '',
                 'One of Sky\'s many fun features is a chat-based Points system.',
                 'Users earn points by chatting in any channel Sky can read.',
@@ -31,9 +31,25 @@ module.exports = {
                 'Say you bet 50 points: you either win, or you lose. Winning would mean you gain 50 points. Losing would mean you lose the 50 points you bet.',
                 `The current points commands consist of: ${pointsCmds.join(' ')}.`,
                 '',
-                `Currently the user with the most Sky Points™ is **${client.points.array().sort((a, b) => b.points - a.points)[0].tag}**`
+                `Currently the user with the most Sky Points™ is **${client.points.array().sort((a, b) => b.points - a.points)[0].tag}**, see if you can overtake them!`
             ].join('\n');
             msg.channel.send(helpPoints);
+        } else if (args[0].toLowerCase() === 'cleverbot' || args[0].toLowerCase() === 'ai') {
+            const helpCleverbot = [
+                '__Help for **Cleverbot**__',
+                'Sky has a built-in Cleverbot AI.',
+                '',
+                'Access to Cleverbot API does infact cost $$ but it is worth it.',
+                'If you have any money to spare and would like to support the',
+                `Cleverbot AI in Sky, please use \`${client.config.prefix}about\` command.`,
+                'Donations are greatly appreciated.',
+                '',
+                'To use the Cleverbot AI, just mention Sky with a query.',
+                'For example -> **@Sky how was your day?**',
+            ].join('\n');
+            msg.channel.send(helpCleverbot);
+        } else {
+            msg.channel.send(`\\❌ Unknown support category. For a list of categories, please use \`${client.config.prefix}help\`.`);
         }
         
     },
