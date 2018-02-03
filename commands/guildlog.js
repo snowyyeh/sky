7f805701063eaf2a5543468e7ede406fe260d4b2
Jellz <xdjellz@gmail.com>
2018-02-04 10:29:18 +1100

    ok fix ID & msg spacing

--- commands/guildlog.js
+++ commands/guildlog.js
@@ -5,10 +5,10 @@ module.exports = {
         const m = await msg.channel.send('<a:skyloading:397962260540293120> Dumping all guilds to file...');
         fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(0, 4)}-guilds.txt`, '--- BEGIN GUILD LOG ---');
         await client.guilds.array().forEach(element => {
-            fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(0, 4)}-guilds.txt`, `NAME: ${element.name} ID: ${element.id} OWNER: ${element.owner.user.tag} (${element.owner.user.id}) SIZE: ${element.memberCount}`);
+            fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(0, 4)}-guilds.txt`, ` NAME: ${element.name} ID: ${element.id} OWNER: ${element.owner.user.tag} (${element.owner.user.id}) SIZE: ${element.memberCount} `);
         });
         fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(0, 4)}-guilds.txt`, '--- END GUILD LOG ---');
-        m.edit(`✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id.slice(0, 4)}-guilds.txt>`);   
+        m.edit(`✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id.slice(4, 7)}-guilds.txt>`);   
     },
     meta: {
         name: 'guildlog',
