7045a022181997404b6e1097c87e51149c13d456
Jellz <xdjellz@gmail.com>
2018-02-03 16:54:32 +1100

    exec command

---
+++ commands/exec.js
@@ -0,0 +1,18 @@
+module.exports = {
+    run: async (client, msg, args) => {
+        if (!args[0]) return msg.channel.send('\\❌ Please provide a command to execute.');
+        require('child_process').exec(args.join(' '), (error, stdout, stderr) => {
+            if (error) {
+                msg.channel.send(error);
+                return console.error(error);
+            }
+            msg.channel.send(`\\📤 **Output:**\n\n${stdout}`);
+        });
+    },
+    meta: {
+        name: 'exec',
+        ownerOnly: true,
+        description: 'Executes a terminal command through the bot.',
+        usage: '<%command%>'
+    }
+}
