e5e77eaa406998ff64411971a040bbdb114ef804
Jellz <xdjellz@gmail.com>
2018-02-04 10:51:57 +1100

    maybe dont spam the console with deprecation errors

--- commands/guildlog.js
+++ commands/guildlog.js
@@ -4,11 +4,11 @@ module.exports = {
     run: async (client, msg, args) => {
         const m = await msg.channel.send('<a:skyloading:397962260540293120> Dumping all guilds to file...');
         fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, '--- BEGIN GUILD LOG ---');
-        await client.guilds.array().forEach(element => {
+        client.guilds.array().forEach(element => {
             fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, ` \nNAME: ${element.name} ID: ${element.id} OWNER: ${element.owner.user.tag} (${element.owner.user.id}) SIZE: ${element.memberCount} `);
         }, (err) => console.error(err));
         fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, '\n--- END GUILD LOG ---');
-        m.edit(`✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id}-guilds.txt>`);   
+        m.edit(`\\✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id}-guilds.txt>`);   
     },
     meta: {
         name: 'guildlog',
