554d1e277af1faf6186f8f2513f11b2b1cd0c347
Jellz <xdjellz@gmail.com>
2018-02-03 14:18:26 +1100

    test avatar command ;o

---
+++ commands/avatar.js
@@ -0,0 +1,12 @@
+module.exports = {
+    run: async (client, msg, args) => {
+        const user = args[0] ? msg.mentions.users.first() || client.users.get(args[0]) : msg.author;
+        msg.channel.send(`😀 Avatar of **${user.tag}**...`, { files: user.avatarURL() });        
+    },
+    meta: {
+        name: 'avatar',
+        ownerOnly: false,
+        description: 'Grabs the target\'s Discord avatar.',
+        usage: '[%mention%|%user ID%]'
+    }
+}
