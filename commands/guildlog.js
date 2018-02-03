7a768838841a702e28f3001c5123cde5336328ee
Jellz <xdjellz@gmail.com>
2018-02-04 10:34:34 +1100

    fix this once and for all

--- commands/guildlog.js
+++ commands/guildlog.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 module.exports = {
     run: async (client, msg, args) => {
         const m = await msg.channel.send('<a:skyloading:397962260540293120> Dumping all guilds to file...');
-        fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(4, 7)}-guilds.txt`, '--- BEGIN GUILD LOG ---');
+        fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, '--- BEGIN GUILD LOG ---');
         await client.guilds.array().forEach(element => {
-            fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(4, 7)}-guilds.txt`, ` NAME: ${element.name} ID: ${element.id} OWNER: ${element.owner.user.tag} (${element.owner.user.id}) SIZE: ${element.memberCount} `);
+            fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, ` NAME: ${element.name} ID: ${element.id} OWNER: ${element.owner.user.tag} (${element.owner.user.id}) SIZE: ${element.memberCount} `);
         }, (err) => console.error(err));
-        fs.appendFile(`/home/jellz/personalcdn/data/${msg.id.slice(4, 7)}-guilds.txt`, '--- END GUILD LOG ---');
-        m.edit(`✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id.slice(4, 7)}-guilds.txt>`);   
+        fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, '--- END GUILD LOG ---');
+        m.edit(`✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id}-guilds.txt>`);   
     },
     meta: {
         name: 'guildlog',
