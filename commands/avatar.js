cad251371a4e88a9099e978af7ad1015772dd371
Jellz <xdjellz@gmail.com>
2018-02-03 14:35:54 +1100

    api limitations :<

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,8 @@ module.exports = {
     run: async (client, msg, args) => {
         const user = args[0] ? msg.mentions.users.first() || client.users.get(args[0]) : msg.author;
         const m = await msg.channel.send(`<a:skyloading:397962260540293120> Grabbing avatar of **${user.tag}**...`, { files: [] });
-        m.edit(`\\😀 Avatar of **${user.tag}**...`, { files: [user.avatarURL()] });        
+        m.delete;
+        msg.channel.send(`\\😀 Avatar of **${user.tag}**...`, { files: [user.avatarURL()] });        
     },
     meta: {
         name: 'avatar',
