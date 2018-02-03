e1d1c31483bbb34c03a194b19b750c44b3128cd1
Jellz <xdjellz@gmail.com>
2018-02-04 10:53:59 +1100

    just dont do any async crap

--- commands/guildlog.js
+++ commands/guildlog.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 
 module.exports = {
     run: async (client, msg, args) => {
-        const m = await msg.channel.send('<a:skyloading:397962260540293120> Dumping all guilds to file...');
         fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, '--- BEGIN GUILD LOG ---');
         client.guilds.array().forEach(element => {
             fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, ` \nNAME: ${element.name} ID: ${element.id} OWNER: ${element.owner.user.tag} (${element.owner.user.id}) SIZE: ${element.memberCount} `);
         }, (err) => console.error(err));
         fs.appendFile(`/home/jellz/personalcdn/data/${msg.id}-guilds.txt`, '\n--- END GUILD LOG ---');
-        m.edit(`\\✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id}-guilds.txt>`);   
+        msg.channel.send(`\\✅ Done! You can view the file @ <https://cdn.jellz.fun/${msg.id}-guilds.txt>`);   
     },
     meta: {
         name: 'guildlog',
