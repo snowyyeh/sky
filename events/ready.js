894848339ab3140f30f6d094f42b1250e355481c
Jellz <xdjellz@gmail.com>
2018-02-05 21:15:53 +1100

    I SWEAR THIS BETTER BE THE LAST ONE

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = {
     run: async (client) => {
         console.log(`Sky: logged in as ${client.user.tag}!`);
-        (await client.db.table('points').run()).forEach(element => element.earningPoints = true);
+        (await client.db.table('globalPoints').run()).forEach(element => element.earningPoints = true);
         console.log('Set all points profiles to EARNING POINTS.');
     }
 }
