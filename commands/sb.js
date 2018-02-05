d8cdcca18e9dcbe571b5879698a09d821623f7d4
Jellz <xdjellz@gmail.com>
2018-02-06 07:40:26 +1100

    ahhaha

--- commands/sb.js
+++ commands/sb.js
@@ -11,7 +11,7 @@ module.exports = {
             const voiceChannel = msg.member.voiceChannel;
             voiceChannel.join()
             .then(connection => {
-                const dispatcher = connection.playFile(resolve(`./sounds/${args[0].toLowerCase()}.mp3`));
+                const dispatcher = connection.playFile(require('path').resolve(`./sounds/${args[0].toLowerCase()}.mp3`));
                 dispatcher.on('end', () => {
                     voiceChannel.leave();
                 });
