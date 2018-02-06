921cf50648a54759e0bf2ca6422e5bcece47e4ff
Jellz <xdjellz@gmail.com>
2018-02-06 19:17:03 +1100

    ok lets not subtract 1 and just lie to the end user about there being 1 less user

--- commands/about.js
+++ commands/about.js
@@ -4,7 +4,7 @@ module.exports = {
             '__About **Sky**__',
             `Sky is in **${client.guilds.size}** guilds.`,
             `Sky is monitoring **${client.channels.size}** channels.`,
-            `Sky is playing with **${client.users.size - 1}** other users.`
+            `Sky is playing with **${client.users.size}** other users.`
             `Sky is running Node.js version **${process.version}**.`,
             `Sky is running Discord.js version **${require('discord.js').version}.**`,
             `Sky is looking for new users! Use \`;help\` to find out how you can use Sky.`
