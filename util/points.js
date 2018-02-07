a6ff9190b61e0c9fef697df0f0ab59e25cd51082
Jellz <xdjellz@gmail.com>
2018-02-08 07:20:02 +1100

    make it a multiplier system

--- util/points.js
+++ util/points.js
@@ -1,12 +1,12 @@
 module.exports = {
-    run: async (client, user, amtOfPoints = 2) => {
+    run: async (client, user, multiplier = 1) => {
         const r = client.db;
         if (!await r.table('globalPoints').get(user.id).run()) {
             require('./insertNewPointsUser.js').run(client, user);
         }
         const points = await r.table('globalPoints').get(user.id).run();
         if (points.earningPoints) {
-            points.points = points.points + amtOfPoints;
+            points.points = points.points * multiplier;
             points.earningPoints = false;
             await r.table('globalPoints').get(user.id).update(points).run();
             setTimeout(async function() {
