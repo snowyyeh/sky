bc63f8db1cdd2080c4d29eda7e0a3520504deca9
Jellz <xdjellz@gmail.com>
2018-02-07 20:52:44 +1100

    add a lil' note about global leaderboard to ;lb

--- commands/lb.js
+++ commands/lb.js
@@ -3,6 +3,7 @@ module.exports = {
         const r = client.db;
         const leaderboard = [];
         leaderboard.push('*The leaderboard was recently reset, now\'s your chance to get to the top!*');
+        leaderboard.push('*The leaderboard is global across all guilds Sky is in.');
         leaderboard.push('**__DISPLAYING TOP 10 USERS__**');
         var place = 0;
         const lb = (await r.table('globalPoints').run()).sort((a, b) => b.points - a.points).slice(0, 10)
