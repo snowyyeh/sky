10cdb7aec8ff727b33e3d5715be89888753ab7f3
Jellz <xdjellz@gmail.com>
2018-02-10 21:49:21 +1100

    Incorrect website directory

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
             client.tempProfiles[element.id] = { earningPoints: true, allowedToBet: true };
         });
         console.log('Set all points profiles to EARNING POINTS.');
-        require('./website/website.js').app.listen(process.env.PORT || 3003, function() {
+        require('../website/website.js').app.listen(process.env.PORT || 3003, function() {
             console.log(`Listening on port ${process.env.PORT || 3003}.`);
         });
         
