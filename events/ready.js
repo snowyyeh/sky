c328c62f686758102399b0dfa9392f9d1b40efdf
Jellz <xdjellz@gmail.com>
2018-02-20 17:48:30 +1100

    remove allowedToBet property from temp profiles

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
     run: async (client) => {
         console.log(`Sky: logged in as ${client.user.tag}!`);
         (await client.db.table('globalPoints').run()).forEach(element => {
-            client.tempProfiles[element.id] = { earningPoints: true, allowedToBet: true };
+            client.tempProfiles[element.id] = { earningPoints: true };
         });
         console.log('Set all points profiles to EARNING POINTS.');
         require('../website/website.js').app.listen(process.env.PORT || 3003, function() {
