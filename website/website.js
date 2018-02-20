4744245fd32ad6d1acbe0bfcd8a15f2835acfe9b
Jellz <xdjellz@gmail.com>
2018-02-20 18:40:22 +1100

    test ID requirement

--- website/website.js
+++ website/website.js
@@ -6,6 +6,11 @@ app.get('/', async (req, res) => {
     res.status(200).sendFile(__dirname + '/static/index.html');
 });
 
+app.get('/u/:id', async (req, res) => {
+    const client = require('../index.js').client;
+    if (!req.params.id) return res.send('Id required');
+});
+
 app.use(function(req, res) {
     res.status(404).sendFile(__dirname + '/static/404.html');
 });
