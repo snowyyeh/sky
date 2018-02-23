10fd326234cd3ee29b11916a8a06697199fd8583
Jellz <xdjellz@gmail.com>
2018-02-24 10:27:55 +1100

    add presence update on ready event

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 module.exports = {
     run: async (client) => {
         console.log(`Sky: logged in as ${client.user.tag}!`);
+        client.user.setActivity(';help | https://jellz.fun/sky');
         (await client.db.table('globalPoints').run()).forEach(element => {
             client.tempProfiles[element.id] = { earningPoints: true };
         });
