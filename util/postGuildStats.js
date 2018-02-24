a7f2fadd62bace7e55b9751b734ff445e2f2fa0b
Jellz <xdjellz@gmail.com>
2018-02-24 22:34:45 +1100

    stop spamming those poor apis (sorry apis)

--- util/postGuildStats.js
+++ util/postGuildStats.js
@@ -1,6 +1,9 @@
 module.exports = {
     run: async (client) => {
         const snek = require('snekfetch');
+
+        var prevGuild = prevGuild ? prevGuild : client.guilds.size;
+        if (prevGuild == client.guilds.size) return console.log('Did not send API request due to same amount of guilds!');
         
         // Sending to bots.discord.pw
         const dbotsRes = await snek.post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
@@ -18,5 +21,6 @@ module.exports = {
         dblRes.postStats(client.guilds.size);
 
         console.log('Successfully posted guild count to 3 bot lists');
+        prevGuild = client.guilds.size;
     }
 }
