b0544427a7ea30b455027f6bddcb5f89e0ec9790
Jellz <xdjellz@gmail.com>
2018-03-02 22:53:10 +1100

    web changes and guild posting fixes

--- util/postGuildStats.js
+++ util/postGuildStats.js
@@ -2,8 +2,8 @@ module.exports = {
     run: async (client) => {
         const snek = require('snekfetch');
 
-        var prevGuild = prevGuild ? prevGuild : client.guilds.size;
-        if (prevGuild == client.guilds.size) return console.log('Did not send API request due to same amount of guilds!');
+        // var prevGuild = prevGuild ? prevGuild : client.guilds.size;
+        // if (prevGuild == client.guilds.size) return console.log('Did not send API request due to same amount of guilds!');
         
         // Sending to bots.discord.pw
         const dbotsRes = await snek.post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
@@ -20,7 +20,7 @@ module.exports = {
         const dblRes = new DBL(client.config.dblToken);
         dblRes.postStats(client.guilds.size);
 
-        console.log('Successfully posted guild count to 3 bot lists');
-        prevGuild = client.guilds.size;
+        // console.log('Successfully posted guild count to 3 bot lists');
+        // prevGuild = client.guilds.size;
     }
 }
