171aedd420cdd83a7027eeaaf8ee647a6bb7b2aa
Jellz <xdjellz@gmail.com>
2018-03-02 22:12:52 +1100

    debug time

--- commands/cb.js
+++ commands/cb.js
@@ -2,9 +2,11 @@ module.exports = {
     run: async (client, msg, args) => {
         if (!args[0]) return msg.channel.send('\\❌ Please provide a valid input.');
         const query = args.join(' ');
+        msg.channel.send(`query: ${query}`);
         const cleverbot = require('../index.js').cleverbot;
         const response = await cleverbot.write(query, function() {
             msg.channel.send(response.output);
+            msg.channel.send(JSON.stringify(response), { code: 'json' });
         });
     },
     meta: {
