a684c5513b4cf8faff44f4123da81bb810d1eaea
Jellz <xdjellz@gmail.com>
2018-03-02 22:24:05 +1100

    make a loading thingo

--- commands/cb.js
+++ commands/cb.js
@@ -2,9 +2,10 @@ module.exports = {
     run: async (client, msg, args) => {
         if (!args[0]) return msg.channel.send('\\❌ Please provide a valid input.');
         const query = args.join(' ');
+        const m = await msg.channel.send('<a:skyloading:397962260540293120> Processing input...');
         const cleverbot = require('../index.js').cleverbot;
         await cleverbot.write(query, async function(response) {
-            msg.channel.send(`**${msg.author.username}**, ${response.output}`);
+            m.edit(`**${msg.author.username}**, ${response.output}`);
         });
     },
     meta: {
