338de3528ba12a91080394610b8e99c35d379c3c
Jellz <xdjellz@gmail.com>
2018-03-02 22:19:27 +1100

    cleverbot works! (remove debug)

--- commands/cb.js
+++ commands/cb.js
@@ -2,11 +2,9 @@ module.exports = {
     run: async (client, msg, args) => {
         if (!args[0]) return msg.channel.send('\\❌ Please provide a valid input.');
         const query = args.join(' ');
-        msg.channel.send(`query: ${query}`);
         const cleverbot = require('../index.js').cleverbot;
-        await cleverbot.write(query, function(response) {
-            msg.channel.send(response.output);
-            msg.channel.send(JSON.stringify(response), { code: 'json' });
+        await cleverbot.write(query, async function(response) {
+            msg.channel.send(`**${msg.author.username}**, ${response.output}`);
         });
     },
     meta: {
