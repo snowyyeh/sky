c867252145a82e25afc965ea0bf7e544134e729f
Daniel <24366082+jellz@users.noreply.github.com>
2018-03-09 17:49:29 +1100

    Fix cat command as Riley's code was erroneous

--- commands/cat.js
+++ commands/cat.js
@@ -2,9 +2,8 @@ const snek = require('snekfetch');
 
 module.exports = {
     run: async (client, msg, args) => {
-        let m = await msg.channel.send('<a:skyloading:397962260540293120> Fetching a random cat... Just one sec! 🐱');
         const r = await snek.get('http://random.cat/meow');
-        m.edit('🐱 Here you go...', { files: [r.body.file] });
+        msg.channel.send('Here is a random cat picture...', { files: [r.body.file] });
     },
     meta: {
         name: 'cat',
