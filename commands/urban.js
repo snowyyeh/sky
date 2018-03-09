8e24726c227b80c512c5ac1e2c1a5280f40ba5b5
Snowyy <37204726+snowyyeh@users.noreply.github.com>
2018-03-09 18:05:55 +1000

    dupe code?

--- commands/urban.js
+++ commands/urban.js
@@ -7,10 +7,7 @@ module.exports = {
         const m = await msg.channel.send(`<a:skyloading:397962260540293120> Getting definition for \`${args.join(' ')}\`..`);    
         const { body } = await snekfetch.get('https://api.urbandictionary.com/v0/define').query({ term: args.join(' ') });        
         if (body.result_type === 'no_results') return m.edit(`\\❌ No results found.`);        
-        const [answer] = body.list;
-        const m = await msg.channel.send(`<a:skyloading:397962260540293120> Getting definition for \`${args.join(' ')}\`...`);    
-        const { body } = await snek.get('https://api.urbandictionary.com/v0/define').query({ term: args.join(' ') });        
-        if (body.result_type == 'no_results') return m.edit(`\\❌ No results found.`);        
+        const [answer] = body.list;     
         m.edit(`__**Word:**__ ${args.join(' ')}\n\n__**Definition:**__\n${trim(answer.definition, 1024)}\n\n__**Example(s):**__\n${trim(answer.example, 1024)}\n\n__**Rating:**__ \\👍 ${answer.thumbs_up} thumbs up. \\👎 ${answer.thumbs_down} thumbs down.\n\n__**Tags:**__\n${body.tags.join(', ')}`);
     },
     meta: {
